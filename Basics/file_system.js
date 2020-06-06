d6e12915886747d6d23b33bb5ea4889c196450be
Harsh Mule <hmuley22@gmail.com>
2020-06-06 16:17:52 +0530

    basics and urls query programs

--- Basics/file_system.js
+++ Basics/file_system.js
@@ -1,5 +1,7 @@
 var http = require('http');
+var fs = require('fs');
+
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
-}).listen(8080);
+    res.end();
+})
