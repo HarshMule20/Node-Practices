60254402fe18bbb0dd3814a187c50cd88e98aaa7
Harsh Mule <hmuley22@gmail.com>
2020-06-09 17:24:24 +0530

    removing the extension from the url

--- Basics/remove_html.js
+++ Basics/remove_html.js
@@ -4,18 +4,22 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
-    var page_file = "." + q.pathname;
-    if (page_file == './'){
-        page_file = "./app.html"; 
-    } 
-    fs.readFile(page_file, function(err, data){
+    var page_name = "." + q.pathname;
+    if (page_name == './'){
+        page_name = './app';
+    }
+
+    page_name = page_name + '.html';
+    console.log(page_name);
+    fs.readFile(page_name, function(err, data){
         if (err){
             res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end();
+            return res.end("<h1>404 Not Found</h1>")
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     })
-}).listen(8080)
-console.log("Listening to the port 8080.....");
+}).listen(8080);
+
+console.log("Listening to port 8080....");
